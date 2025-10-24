12e97b9ba9e4fdec3f541c56e088b88fe1b2c0a8
joelsilva444 <joelpitra88@gmail.com>
2025-10-24 17:55:15 +0100

    feat: update

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "8080",
-        pathname: "/public/avatars/**",
-      },
-      {
-        protocol: "https",
-        hostname: "anonimo-angola-api.onrender.com",
-        port: "",
-        pathname: "/public/avatars/**",
-      },
-    ],
-  },
-};
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "8080",
+				pathname: "/public/avatars/**",
+			},
+			{
+				protocol: "https",
+				hostname: "anonimo-angola-api-two.vercel.app",
+				port: "",
+				pathname: "/public/avatars/**",
+			},
+		],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
